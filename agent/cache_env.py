708506c0a6003f8976ed66cb7aa34feeebdfbdc7
“wyang” <wyang107@ucr.edu>
2026-03-19 23:38:33 -0400

    result path

--- agent/cache_env.py
+++ agent/cache_env.py
@@ -100,6 +100,7 @@ class CacheEnv:
                 max_query_ids=self.max_query_ids,
                 max_query_fields=self.max_query_fields,
                 extra_query_num=self.extra_query_num,
+                global_check_alpha=self.global_check_alpha,
                 tool_failure_rate=tool_failure_rate,
                 trial_index=trial_index,
             )
@@ -234,6 +235,7 @@ class CacheEnv:
         max_query_ids: int,
         max_query_fields: int,
         extra_query_num: int,
+        global_check_alpha: float | None,
         tool_failure_rate: float,
         trial_index: int,
     ) -> str:
@@ -241,14 +243,10 @@ class CacheEnv:
             f"fail-{tool_failure_rate}_"
             f"trial-{trial_index}.json"
         )
+        alpha_str = "none" if global_check_alpha is None else str(global_check_alpha)
+        config_dir = f"ids{max_query_ids}_fields{max_query_fields}_eq{extra_query_num}_alpha{alpha_str}"
         model_dir_name = self._get_model_dir_name(model_name)
-        result_instance_id = self._build_result_instance_id(
-            instance_id=instance_id,
-            max_query_ids=max_query_ids,
-            max_query_fields=max_query_fields,
-            extra_query_num=extra_query_num,
-        )
-        return os.path.join(save_root, model_dir_name, result_instance_id, file_name)
+        return os.path.join(save_root, config_dir, model_dir_name, instance_id, file_name)
 
     def _save_run_result(
         self,
